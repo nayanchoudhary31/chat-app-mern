c142accfe6856ff6f39216e1f9da220d1b8248c7
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-28 00:03:42 +0530

    updated-server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const userRoutes = require("./routes/userRoutes");
+app.use(express.json())
+const PORT = process.env.PORT;
 
+app.use('/api/user',userRoutes);
+
+// app.get('/',(req,resp)=>{
+//     resp.status(200).json({message:"Hello"})
+// })
 
 app.listen(PORT,()=>{
     console.log(`Running server on port ${PORT}`)
