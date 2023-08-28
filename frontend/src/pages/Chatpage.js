c18340852ff63d2cbe0fab53e3992cfdc7f6d5eb
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-28 18:21:17 +0530

    added-frontend

---
+++ frontend/src/pages/Chatpage.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+const Chatpage = () => {
+  const fetchData = async () => {
+    const { data } = await axios.get('/api/chats')
+    console.log('🚀 ~ file: Chatpage.js:8 ~ fetchData ~ data:', data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  return <div>Chatpage</div>
+}
+
+export default Chatpage
---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,43 @@
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
+import React from 'react'
+import Login from '../components/authentication/Login'
+import Signup from '../components/authentication/Signup'
+
+const Homepage = () => {
+  return (
+    <Container maxW="xl">
+      <Box
+        display="flex"
+        justifyContent="center"
+        padding={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work Sans" color="black">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box bg={'white'} w="100%" p={4} borderRadius={'lg'} color={'black'} borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList marginBottom={'1em'}>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  )
+}
+
+export default Homepage
