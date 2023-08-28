feae36f08e2a10093cb161079aee09e28f2b9feb
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-28 18:18:29 +0530

    updated-server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const userRoutes = require("./routes/userRoutes");
+const express = require('express')
+const app = express()
+const connectDb = require('./config/db')
+require('dotenv').config()
+const userRoutes = require('./routes/userRoutes')
+const { chats } = require('./data/chat')
+connectDb()
 app.use(express.json())
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
-app.use('/api/user',userRoutes);
+app.get('/', (req, resp) => {
+  resp.json({ message: 'API is running' })
+})
 
-// app.get('/',(req,resp)=>{
-//     resp.status(200).json({message:"Hello"})
-// })
+app.get('/api/chats', (req, resp) => {
+  resp.send(chats)
+})
+app.use('/api/user', userRoutes)
 
-app.listen(PORT,()=>{
-    console.log(`Running server on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Running server on port ${PORT}`)
 })
