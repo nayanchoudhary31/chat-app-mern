90f69183033d5eb1573a30870efa12f0dd43583f
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-29 14:48:38 +0530

    updated-controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,64 @@
+const { generateToken } = require('../config/generateToken')
+const User = require('../model/userModel')
 const asyncHandler = require('express-async-handler')
 
-const userRegister = asyncHandler(async () => {})
+const userRegister = asyncHandler(async (req, resp) => {
+  const { name, email, password, pic } = req.body
 
-const loginUserAuth = asyncHandler(async () => {})
+  if (!name || !email || !password) {
+    resp.status(400)
+    throw new Error('All fields are mandatory!')
+  }
+
+  const isUserExist = await User.findOne({ email })
+  if (isUserExist) {
+    resp.status(400)
+    throw new Error('User already exist!')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  })
+
+  if (user) {
+    resp.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    })
+  } else {
+    resp.status(400)
+    throw new Error('User not found')
+  }
+})
+
+const loginUserAuth = asyncHandler(async (req, resp) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    resp.status(400)
+    throw new Error('Email & Password is required !')
+  }
+
+  const user = await User.findOne({ email })
+
+  if (user && user.validatePassword(password)) {
+    resp.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    })
+  } else {
+    resp.status(401)
+    throw new Error('Invalid Email or Password')
+  }
+})
 
 module.exports = { userRegister, loginUserAuth }
--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,15 @@ const connectDb = require('./config/db')
 require('dotenv').config()
 const userRoutes = require('./routes/userRoutes')
 const { chats } = require('./data/chat')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 connectDb()
 app.use(express.json())
 const PORT = process.env.PORT
 
-app.get('/', (req, resp) => {
-  resp.json({ message: 'API is running' })
-})
-
-app.get('/api/chats', (req, resp) => {
-  resp.send(chats)
-})
 app.use('/api/user', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Running server on port ${PORT}`)
