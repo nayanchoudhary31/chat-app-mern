a74b34d2a35cd88c6e0172600bf928b8f99d7a9d
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-31 19:38:45 +0530

    chat-page

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const Chatpage = () => {
   const fetchData = async () => {
-    const { data } = await axios.get('/api/chats')
-    console.log('🚀 ~ file: Chatpage.js:8 ~ fetchData ~ data:', data)
+    // const { data } = await axios.get('/api/chats')
+    // console.log('🚀 ~ file: Chatpage.js:8 ~ fetchData ~ data:', data)
   }
   useEffect(() => {
     fetchData()
