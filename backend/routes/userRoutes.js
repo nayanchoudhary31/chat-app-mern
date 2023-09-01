c42b40168f1d899d0b3ae0e0cfb3c557d1db4058
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-09-01 17:20:51 +0530

    added-search-route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { userRegister, loginUserAuth } = require('../controller/userController')
+const { userRegister, loginUserAuth, getAllUser } = require('../controller/userController')
+const authToken = require('../middleware/authMiddleware')
 
-router.route('/').post(userRegister)
+router.route('/').post(userRegister).get(authToken, getAllUser)
 router.route('/login').post(loginUserAuth)
 
 module.exports = router
