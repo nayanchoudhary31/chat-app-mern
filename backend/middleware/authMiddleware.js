58eac320f0cb934726c717925c28baa9f86015b6
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-09-01 17:22:39 +0530

    added authmiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../model/userModel')
+require('dotenv').config()
+
+const authToken = asyncHandler(async (req, resp, next) => {
+  let token
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+
+      // decode the token and get the user detail
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+      req.user = await User.findById(decoded.userId).select('-passoword')
+
+      next()
+    } catch (error) {
+      resp.status(401)
+      throw new Error('Not authorized, token failed')
+    }
+  }
+
+  if (!token) {
+    resp.status(401)
+    throw new Error('Not authorized, no token')
+  }
+})
+
+module.exports = authToken
