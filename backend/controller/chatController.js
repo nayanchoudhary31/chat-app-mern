ded2c9289fecf97fd70e8082217a29f0d0fc7c04
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-09-16 22:55:02 +0530

    added-chat-one2one

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -1,7 +1,51 @@
 const asyncHandler = require('express-async-handler')
+const Chat = require('../model/chatModel')
+const User = require('../model/userModel')
 const getAllChat = asyncHandler(async (req, resp) => {})
 
-const accessChat = asyncHandler(async (req, resp) => {})
+const accessChat = asyncHandler(async (req, resp) => {
+  const { userId } = req.body
+
+  if (!userId) {
+    console.log(`UserId params not sent with request`)
+    return resp.sendStatus(400)
+  }
+  let isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      {
+        users: { $elemMatch: { $eq: req.user._id } },
+        users: { $elemMatch: { $eq: userId } },
+      },
+    ],
+  })
+    .populate('users', '-password')
+    .populate('latestMessage')
+
+  isChat = await User.populate(isChat, {
+    path: 'latestMessage.sender',
+    select: 'name pic email',
+  })
+
+  if (isChat.length > 0) {
+    resp.send(isChat[0])
+  } else {
+    var chatData = {
+      chatName: 'sender',
+      isGroupChat: false,
+      users: [req.user, userId],
+    }
+  }
+
+  try {
+    const createdChat = await Chat.create(chatData)
+    const fullChat = await Chat.findOne({ _id: createdChat._id }).populate('users', '-password')
+    resp.status(200).json(fullChat)
+  } catch (error) {
+    resp.status(400)
+    throw new Error(error.message)
+  }
+})
 
 const createGroupChat = asyncHandler(async (req, resp) => {})
 const addToGroup = asyncHandler(async (req, resp) => {})
