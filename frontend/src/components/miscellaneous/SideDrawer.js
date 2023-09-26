d64bd684a19c2c172e0d5d159040b861885d061b
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-09-26 22:55:51 +0530

    worked on side drawer

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,30 @@
-import { Avatar, Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react'
 import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import React, { useState } from 'react'
 import { ChatState } from '../../context/ChatProvider'
+import ProfileModal from './ProfileModal'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+import ChatLoading from '../ChatLoading'
 
 const SideDrawer = () => {
   const [search, setSearch] = useState('')
@@ -9,6 +32,49 @@ const SideDrawer = () => {
   const [loading, setLoading] = useState(false)
   const [loadingChat, setLoadingChat] = useState()
   const { user } = ChatState()
+  const history = useHistory()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+  const logOutHandler = () => {
+    localStorage.removeItem('userInfo')
+    history.push('/')
+  }
+
+  const searchHandler = async () => {
+    if (!search) {
+      toast({
+        title: 'Please enter something in search',
+        position: 'top-left',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+      })
+    }
+
+    try {
+      setLoading(true)
+      console.log(user)
+      const config = {
+        headers: {
+          Auhtorization: `Bearer ${user.token}`,
+        },
+      }
+
+      const { data } = await axios.get(`/api/user?search=${search}`, config)
+      setLoading(false)
+      console.log('🚀 ~ file: SideDrawer.js:65 ~ searchHandler ~ data:', data)
+      setSearchResult(data)
+    } catch (error) {
+      toast({
+        title: 'Error Occured',
+        description: 'Failed to load the search results',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-left',
+      })
+    }
+  }
   return (
     <>
       <Box
@@ -21,7 +87,7 @@ const SideDrawer = () => {
         borderWidth="5px"
       >
         <Tooltip label="Search for User to chat" hasArrow placement="bottom-end">
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={onOpen}>
             <i class="fa-solid fa-magnifying-glass"></i>
             <Text display={{ base: 'none', md: 'flex' }} px="4">
               Search Users
@@ -42,13 +108,36 @@ const SideDrawer = () => {
               <Avatar src={user.pic} size={'sm'} cursor={'pointer'} name={user.name} />
             </MenuButton>
             <MenuList>
-              <MenuItem>Profile</MenuItem>
+              <ProfileModal user={user}>
+                <MenuItem>Profile</MenuItem>
+              </ProfileModal>
               <MenuDivider />
-              <MenuItem>Logout</MenuItem>
+              <MenuItem onClick={logOutHandler}>Logout</MenuItem>
             </MenuList>
           </Menu>
         </Box>
       </Box>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth={'1px'}>Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box display={'flex'} pb={2}>
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value)
+                }}
+              />
+
+              <Button onClick={searchHandler}>Go</Button>
+            </Box>
+            {loading ? <ChatLoading /> : <span>result</span>}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   )
 }
